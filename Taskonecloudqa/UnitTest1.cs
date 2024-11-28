4b519dbb2a932c549ee2018c5ce8c22b08251048
adibaqr <adibaqureshi1028@gmail.com>
2024-11-28 21:19:35 +0530

    Code changes for test run even if the position changes.

--- Taskonecloudqa/UnitTest1.cs
+++ Taskonecloudqa/UnitTest1.cs
@@ -1,49 +1,67 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using NUnit.Framework;
-namespace Taskonecloudqa
+
+namespace TestProject2
 {
     public class Tests
     {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); 
         }
+
         [Test]
-        public void Test1()
+        public void TestFormFields()
         {
-            IWebDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://app.cloudqa.io/home/AutomationPracticeForm");
             driver.Manage().Window.Maximize();
-            IWebElement webElement = driver.FindElement(By.Name("First Name"));
-            webElement.SendKeys("Ram");
-            webElement.SendKeys(Keys.Return);
-            IWebElement lastName = driver.FindElement(By.Name("Last Name"));
+
+            IWebElement firstName = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//label[text()='First Name']/following-sibling::input")));
+            firstName.SendKeys("Ram");
+
+           
+            IWebElement lastName = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//label[text()='Last Name']/following-sibling::input")));
             lastName.SendKeys("Sharma");
 
+            IWebElement usernameField = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@placeholder='Username' and @name='Username' and @type='text']")));
+            usernameField.SendKeys("RamSharma123");
 
-            IWebElement genderMale = driver.FindElement(By.CssSelector("input[name='gender'][value='Male']"));
+            IWebElement passwordField = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@type='password' and @placeholder='Password' and @name='Password']")));
+            passwordField.SendKeys("StrongPassword123");
+
+            
+            IWebElement confirmPasswordField = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//input[@type='password' and @placeholder='Confirm Password' and @name='Confirm Password']")));
+            confirmPasswordField.SendKeys("StrongPassword123");
+
+            
+            IWebElement genderMale = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//label[text()='Gender']/following-sibling::div//input[@value='Male']")));
             genderMale.Click();
 
-            IWebElement dobField = driver.FindElement(By.Name("Date of Birth"));
-            dobField.SendKeys("2001-01-01");
-            IWebElement usernameField = driver.FindElement(By.Name("Username"));
-            usernameField.SendKeys("RamSharma123");
-            IWebElement passwordField = driver.FindElement(By.Name("Password"));
-            passwordField.SendKeys("RAmsharma@123");
-            IWebElement confirmPasswordField = driver.FindElement(By.Name("Confirm Password"));
-            confirmPasswordField.SendKeys("RAmsharma@123");
-            IWebElement agreeCheckbox = driver.FindElement(By.Name("Agree"));
+            
+            IWebElement dobField = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("dob")));
+            dobField.SendKeys("2002-01-01");
+
+            
+            IWebElement agreeCheckbox = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//input[@type='checkbox' and @id='Agree' and @value='Agree' and @name='Agree']")));
             if (!agreeCheckbox.Selected)
             {
                 agreeCheckbox.Click();
             }
 
-
-            IWebElement submitButton = driver.FindElement(By.Id("submit"));
+            
+            IWebElement submitButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//button[@id='submit']")));
             submitButton.Click();
         }
 
+        
     }
-
 }
+
